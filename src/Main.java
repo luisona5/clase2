831b285a618a312f0dfb07411efe1b51473cb4bf
luisona5 <zoonajluis@gmail.com>
2024-10-10 09:27:00 -0500

    cambiando año

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("'\n\n\n--------LOS LIBRO DE LA BIBLIOTECA SABROSON -----------\n");
+        /*System.out.printf("'\n\n\n--------LOS LIBRO DE LA BIBLIOTECA SABROSON -----------\n");
         // para declarar el objeto
         // para instamciar una clase
         Libro libro1=new Libro("stephen king","el resplandor",600,"de bolsillo");
@@ -23,24 +23,37 @@ public class Main {
         libro4.imprimir();
         System.out.println(libro4.imprimir_Cubiertas());
         libro5.imprimir();
-        System.out.println(libro5.imprimir_Cubiertas());
+        System.out.println(libro5.imprimir_Cubiertas());*/
 
 
         System.out.println("----------------------------------------------MUSICA-----------------------\n");
         Cancion cancion1=new Cancion("payaso","regueton",3.25,"Hector el ¨FATHER¨","GOLD STAR");
         Cancion cancion2=new Cancion("sola","regueton",2.45,"Hector el ¨FATHER¨","GOLD STAR");
         Cancion cancion3=new Cancion("dejalo caer todo el peso","regueton",4.35,"Hector el ¨FATHER¨ft Yomo","Sangre nueva");
+        Cancion cancion4=new Cancion("duele", "pop",5.34,"jessy y joy","un besito mas");
 
         cancion1.imprimir();
         cancion2.imprimir();
         cancion3.imprimir();
+        cancion4.imprimir();
+
 
         System.out.println("----------------------------------------------SERIES-----------------------\n");
         Series series1=new Series("ranma 1/2","familiar",1987,"Japon",35.6);
         Series series2=new Series("los gargolas","familiar",1997,"Estados Unidos",40.5);
+        Series series3=new Series("yugi-OH","familiar",1998,"Japon ",40.5);
 
+        System.out.println(series1.getName());
+        // voy a setear el valor=cambiar el año e este caso
+        System.out.println(series2.getYear());
+        series1.setYear(2014);
+        System.out.println(series1.getYear());
+
+        /*
         series1.imprimir();
         series2.imprimir();
+        series3.imprimir();
+        */
 
 
     }
--- src/Series.java
+++ src/Series.java
@@ -11,7 +11,53 @@ public class Series {
         this.year = year;
         this.country = country;
         this.price = price;
+
     }
+    //----------construyendo los getters
+    public String getName() {
+        return name;
+    }
+    public String getGenre(){
+        return genre;
+    }
+    public int getYear(){
+        return year;
+    }
+    public String getCountry(){
+        return country;
+    }
+    public Double getPrice(){
+        return price;
+    }
+
+    //-------------------------------SETTER
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+
+
+
+
+
+
+
+
+    /*
     public void imprimir(){
         System.out.println("nombre: "+this.name);
         System.out.println("genero: "+this.genre);
@@ -20,4 +66,6 @@ public class Series {
         System.out.println("duracion: "+this.price);
         System.out.println("\n-------------------------------- ");
     }
+    */
+
 }
