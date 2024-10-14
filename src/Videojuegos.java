b89d119a87fe56c893d96f795b8d115147a81c2b
Luis <zoonajluis@gmail.com>
2024-10-14 11:54:41 -0500

    musica

--- src/Videojuegos.java
+++ src/Videojuegos.java
@@ -11,43 +11,48 @@ public class Videojuegos {
         this.desarrollador = desarrollador;
     }
 
-    public Videojuegos() {
-    }
+    // construyendo una clase vacia
+
+    public Videojuegos() {}
+
+    // construyendo los getters--------
 
     public String getNombre() {
         return nombre;
     }
-
     public String getGenero() {
         return genero;
     }
-
     public int getAnio() {
         return anio;
     }
-
     public String getDesarrollador() {
         return desarrollador;
     }
 
-
+    //-------------construyendo los setters----------
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public void setGenero(String genero) {
         this.genero = genero;
     }
-
     public void setAnio(int anio) {
         this.anio = anio;
     }
-
     public void setDesarrollador(String desarrollador) {
         this.desarrollador = desarrollador;
     }
 
-    //-----------metodo personalizados
+    //-----------metodo personalizados-------------
+
+    public void demostrar(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Genero: " + genero);
+        System.out.println("Anio: " + anio);
+        System.out.println("Desarrollador: " + desarrollador);
+
+    }
 
 
 }
